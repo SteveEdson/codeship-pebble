f2e17f4fce12f6c92946635517b74cac804be824
Steve Edson <steve3d50n@gmail.com>
2016-03-01 12:54:05 +0000

    show loading states

--- src/app.js
+++ src/app.js
@@ -9,6 +9,21 @@ var Vector2 = require('vector2');
 var ajax = require('ajax');
 var Settings = require('settings');
 
+if(typeof Settings.option('api_key') == "undefined") {
+    var card = new UI.Card({
+        title: 'API Key Missing',
+        body:  'Add your Codeship API key through the settings page.'
+    });
+
+    card.show();
+} else {
+    
+    var card = new UI.Card({
+        title: 'Loading...'
+    });
+    
+    card.show();
+    
 ajax({
     url: 'https://codeship.com/api/v1/projects.json?api_key=' + Settings.option('api_key'),
     type: 'json'
@@ -75,22 +90,8 @@ ajax({
      function(error, status, request) {
          console.log('The ajax request failed for projects, URL: https://codeship.com/api/v1/projects/:id.json?api_key=' + Settings.option('api_key'));
      });
+}
 
-// Settings.config(
-//     { url: 'https://steveedson.co.uk/codeship-pebble' },
-//   function(e) {
-//     console.log('opening configurable');
-
-//   },
-//   function(e) {
-//       console.log('closed configurable');
-      
-//       JSON.stringify(e.options);
-      
-//       Settings.data('api_key', e.options.api_key);
-      
-//   }
-// );
 
 Pebble.addEventListener('showConfiguration', function(e) {
   // Show config page
@@ -117,72 +118,3 @@ Pebble.addEventListener('webviewclosed', function(e) {
     console.log('Failed to send config data!');
   });
 });
-
-// Settings.config(
-//   { url: 'https://steveedson.co.uk/codeship-pebble' },
-//   function(e) {
-//     console.log('opening configurable');
-
-//     // Reset color to red before opening the webview
-//     //Settings.option('api_key', 'red');
-//   },
-//   function(e) {
-//     console.log('closed configurable');
-//       Settings.option("api_key", e.options.api_key);
-//   }
-// );
-
-
-// var main = new UI.Card({
-//   title: 'Pebble.js',
-//   icon: 'images/menu_icon.png',
-//   subtitle: 'Hello World!',
-//   body: 'Press any button.',
-//   subtitleColor: 'indigo', // Named colors
-//   bodyColor: '#9a0036' // Hex colors
-// });
-
-// main.show();
-
-// main.on('click', 'up', function(e) {
-//   var menu = new UI.Menu({
-//     sections: [{
-//       items: [{
-//         title: 'Pebble.js',
-//         icon: 'images/menu_icon.png',
-//         subtitle: 'Can do Menus'
-//       }, {
-//         title: 'Second Item',
-//         subtitle: 'Subtitle Text'
-//       }]
-//     }]
-//   });
-//   menu.on('select', function(e) {
-//     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-//     console.log('The item is titled "' + e.item.title + '"');
-//   });
-//   menu.show();
-// });
-
-// main.on('click', 'select', function(e) {
-//   var wind = new UI.Window({
-//     fullscreen: true,
-//   });
-//   var textfield = new UI.Text({
-//     position: new Vector2(0, 65),
-//     size: new Vector2(144, 30),
-//     font: 'gothic-24-bold',
-//     text: 'Text Anywhere!',
-//     textAlign: 'center'
-//   });
-//   wind.add(textfield);
-//   wind.show();
-// });
-
-// main.on('click', 'down', function(e) {
-//   var card = new UI.Card();
-//   card.title('A Card');
-//   card.subtitle('Is a Window');
-//   card.body('The simplest window type in Pebble.js.');
-//   card.show();
-// });
